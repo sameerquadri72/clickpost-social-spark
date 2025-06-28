ca98ac4ac101cb783884c18194245584dcce2ea0
sameerquadri72 <83124933+sameerquadri72@users.noreply.github.com>
2025-06-28 21:28:04 +0530

    Fix Tailwind Typography Plugin Configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type { Config } from "tailwindcss";
 
 export default {
@@ -149,5 +148,8 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [
+		require("tailwindcss-animate"),
+		require("@tailwindcss/typography")
+	],
 } satisfies Config;
